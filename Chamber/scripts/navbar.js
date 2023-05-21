506ece90ebfa37ccc9ec5db3504ba65bc56d13c4
jbatman <jbatman5555@gmail.com>
2023-05-20 22:19:44 -0600

    commerce 5/20

--- Chamber/scripts/navbar.js
+++ Chamber/scripts/navbar.js
@@ -1,5 +1,5 @@
 function myFunction() {
-    var x = document.getElementById("myLinks");
+    var x = document.getElementById("mylinks");
     if (x.style.display === "block") {
       x.style.display = "none";
     } else {
@@ -7,44 +7,3 @@ function myFunction() {
     }
   }
 
-  /* Style the navigation menu */
-.navbar {
-  overflow: hidden;
-  background-color: #333;
-  position: relative;
-}
-
-/* Hide the links inside the navigation menu (except for logo/home) */
-.navbar .links {
-  display: none;
-}
-
-/* Style navigation menu links */
-.navbar a {
-  color: white;
-  padding: 14px 16px;
-  text-decoration: none;
-  font-size: 17px;
-  display: block;
-}
-
-/* Style the hamburger menu */
-.navbar a.icon {
-  background: black;
-  display: block;
-  position: absolute;
-  right: 0;
-  top: 0;
-}
-
-/* Add a grey background color on mouse-over */
-.navbar a:hover {
-  background-color: #ddd;
-  color: black;
-}
-
-/* Style the active link (or home/logo) */
-.active {
-  background-color: #04AA6D;
-  color: white;
-}
