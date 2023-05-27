e7ddbe67a9e4084107f976ceee1af1ef6cc68924
jbatman <jbatman5555@gmail.com>
2023-05-27 15:35:04 -0600

    windchill

---
+++ Chamber/scripts/windchill.js
@@ -0,0 +1,13 @@
+var temp = 76;
+var speed = 7;
+
+var windChill = (35.74 + (0.6215 * temp)) - (35.75 * Math.pow(speed, 0.16)) + (0.4275 * temp *Math.pow(speed, 0.16));
+
+if (temp <= 50 && speed >= 3) {
+    windChill = Math.round(35.74 + (0.6215 * temp)) - Math.round(35.75 * Math.pow(speed, 0.16)) + Math.round(0.4275 * temp * Math.pow(speed, 0.16));
+} else {
+   windChill = "N/A";
+}
+
+
+document.getElementById("wChill").innerHTML = windChill;
