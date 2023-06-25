c3f71c2114f237cbddd766f0f7e8c1ab20d145e0
jbatman <jbatman5555@gmail.com>
2023-06-24 19:25:14 -0600

    grid

---
+++ Chamber/scripts/data.js
@@ -0,0 +1,64 @@
+const url = 'scripts/data.json';
+
+async function getData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    // console.table(data.prophets); 
+    displayCompany(data.companies)
+  }
+  
+
+  const displayCompany = (cardData) => {
+    const cards = document.querySelector('#companyCards'); 
+  
+    cardData.forEach((buisness) => {
+      let card = document.createElement('section');
+      let h2 = document.createElement('h2');
+      let companyAddress = document.createElement('p');
+      let companyNumber = document.createElement('P');
+      let companyWeb = document.createElement('p');
+      let portrait = document.createElement('img');
+  
+
+      h2.textContent = `${buisness.name}`;
+      companyAddress.textContent = ` ${buisness.address}`;
+      companyNumber.textContent = ` ${buisness.phonenumber}`;
+      companyWeb.textContent = ` ${buisness.website}`;
+ 
+      portrait.setAttribute('src', buisness.icon);
+      portrait.setAttribute('alt', `${buisness.name}`);
+      portrait.setAttribute('loading', 'lazy');
+      portrait.setAttribute('width', '340');
+      portrait.setAttribute('height', '440');
+  
+  
+      
+      card.appendChild(portrait);
+      card.appendChild(h2);
+      card.appendChild(companyAddress);
+      card.appendChild(companyNumber);
+      card.appendChild(companyWeb);
+      cards.appendChild(card);
+    }); 
+  } 
+  
+  getData();
+
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+const display = document.querySelector("#companyCards");
+
+// The following code could be written cleaner. How? We may have to simplfiy our HTMl and think about a default view.
+
+gridbutton.addEventListener("click", () => {
+	// example using arrow function
+	display.classList.add("grid");
+	display.classList.remove("list");
+});
+
+listbutton.addEventListener("click", showList); // example using defined function
+
+function showList() {
+	display.classList.add("list");
+	display.classList.remove("grid");
+}
--- scripts/prophets.js
+++ scripts/prophets.js
@@ -39,3 +39,5 @@ async function getProphetData() {
   } 
   
   getProphetData();
+
+  
