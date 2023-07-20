95f17f9a96c462b434fbee783d07427ca5cca92f
jbatman <jbatman5555@gmail.com>
2023-07-19 21:35:20 -0600

    fresh

--- bountifulfoods/scripts/data.js
+++ bountifulfoods/scripts/data.js
@@ -9,10 +9,20 @@ function populateSelect(){
             
     }
   }
+  const datefield = document.querySelector("time");
 
+  // derive the current date using a date object
+  const now = new Date();
+  const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+    now
+  );
+  // "full" or long, medium, short options ... try them
+  
+  
     function show(ele) {
       // get the selected value from <select> element and show it.
       let msg = document.getElementById('msg');
       msg.innerHTML = 'Selected Bird: <b>' + ele.options[ele.selectedIndex].text + '</b> </br>' +
             'ID: <b>' + ele.value + '</b>';
+      document.getElementById("date").msg.innerHTML = fulldate;
     }
