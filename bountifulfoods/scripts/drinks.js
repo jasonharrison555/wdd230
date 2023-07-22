26ce481608a49bc837ba84dcf7c847b94553e940
jbatman <jbatman5555@gmail.com>
2023-07-21 18:46:07 -0600

    fresh

--- bountifulfoods/scripts/drinks.js
+++ bountifulfoods/scripts/drinks.js
@@ -1,28 +1,24 @@
-function populateSelect(){
+function getSelectData(){
   const url = 'scripts/drinks.json';
-
-    let ele = document.getElementById('fruits');
+    let fruit = document.getElementById('fruits');
         for (let i = 0; i < url.length; i++) {
-          // populate select element with json.
-          ele.innerHTML = ele.innerHTML +
+          fruit.innerHTML = fruit.innerHTML +
             '<option value="' + url[i]['ID'] + '">' + url[i]['name'] + '</option>';
             
     }
   }
-  const datefield = document.querySelector("time");
 
-  // derive the current date using a date object
+  const datefield = document.querySelector("time");
   const now = new Date();
   const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
     now
   );
-  // "full" or long, medium, short options ... try them
+
   
   
-    function show(ele) {
-      // get the selected value from <select> element and show it.
-      let msg = document.getElementById('msg');
-      msg.innerHTML = 'Selected Bird: <b>' + ele.options[ele.selectedIndex].text + '</b> </br>' +
-            'ID: <b>' + ele.value + '</b>';
-      document.getElementById("date").msg.innerHTML = fulldate;
+    function show(fruit) {
+      let result = document.getElementById('result');
+      result.innerHTML = 'Selected fruit: ' + fruit.options[fruit.selectedIndex].text +
+            'ID:' + fruit.value;
+      document.getElementById("date").result.innerHTML = fulldate;
     }
--- bountifulfoods/scripts/forecast.js
+++ bountifulfoods/scripts/forecast.js
@@ -1,7 +1,15 @@
 fetch('https://api.openweathermap.org/data/2.5/forecast?q=Carlsbad&exclude=hourly,minutely&units=imperial&appid=89a5dfd4094babce2245c6d363403b06')
-.then(response => response.json())
+.then(fill => fill.json())
 .then(data => {
     for (i=0; i<3;i++){
         document.getElementById("day" + (i+1) + "temp").innerHTML = "temp" + Number(data.list[i].main.temp).toFixed(1)
     }
 });
+
+function displayResults(weatherData) {
+    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+    
+    const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    const desc = weatherData.weather[0].description;
+  
+  }
