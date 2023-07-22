b4f57c93cc78b0c0bce47b4c6564f44f2c41ae19
jbatman <jbatman5555@gmail.com>
2023-07-21 20:19:02 -0600

    ffres

--- bountifulfoods/scripts/lastMod.js
+++ bountifulfoods/scripts/lastMod.js
@@ -4,5 +4,5 @@ const date = new Date(document.lastModified);
 // document.getElementById('lastmod').textContent = new Date().toDateString('en-US', options);
 
 let mod = document.lastModified;
-document.getElementById("lastmod").textContent = mo
+document.getElementBId("lastmod").textContent = mod; 
 
